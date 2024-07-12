071ac3629d1594570a00eb170ff47aed5b51592c
Juan <jdeluca@itba.edu.ar>
2024-07-12 18:05:46 +0200

    Frontend mockup

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -65,8 +65,42 @@ function App() {
         </Text>
         <ConnectButton />
       </Flex>
-      <Box maxW="1280px" m="0 auto" p="2rem" textAlign="center">
-        <Flex justifyContent={"center"} my="4">
+      <Box textAlign="center" className="box-container">
+        <div className="left">
+          <div className="container top-left">
+            <h1>Game rules</h1>
+            <ul>
+              <li>
+                Default Retirement Age increases every time someone retires.
+              </li>
+              <li>People that get to the age compete for the slot.</li>
+              <li>
+                The slot goes to the one that got the most money in out of the
+                applicants.
+              </li>
+              <li>The ones that don’t get the slot have to start over.</li>
+              <li>The more people you bring in, the more money you make.</li>
+              <li>
+                The more money you contribute, the earlier you can retire.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="right">
+          <div className="container top-rigt">
+            <h1>Player stuatus</h1>
+            <h2>Retirement age: 65</h2>
+            <h2>Your age: 20</h2>
+            <h2>Your contributions: $120,000</h2>
+            <h2>People brought in: 10</h2>
+          </div>
+          <div className="container bottom-right">
+            <h1>Actions</h1>
+            <button>Contribute more</button>
+            <button>Invite</button>
+          </div>
+        </div>
+        {/* <Flex justifyContent={"center"} my="4">
           <Link href="https://vitejs.dev" isExternal>
             <Image
               src={viteLogo}
@@ -111,7 +145,7 @@ function App() {
         </Box>
         <Text mt="4" color={textColor}>
           Click on the Vite and React logos to learn more
-        </Text>
+        </Text> */}
       </Box>
     </>
   );
