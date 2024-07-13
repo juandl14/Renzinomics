6ec1f881dc0ca9248fd17238e10dea8411de776a
Juan <jdeluca@itba.edu.ar>
2024-07-13 17:15:06 +0200

    semi-finished home

--- apps/web/src/pages/Home/Home.tsx
+++ apps/web/src/pages/Home/Home.tsx
@@ -1,10 +1,117 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  NumberInput,
+  NumberInputField,
+} from "@chakra-ui/react";
+import CFAv1ForwarderABI from "../../assets/CFAv1ForwarderABI/CFAv1ForwarderABI.json";
 import "./Home.css";
+import { useAccount, useWriteContract } from "wagmi";
+import { ChangeEvent, useEffect, useState } from "react";
+
+const CFAv1ForwarderAddress = "0x2CDd45c5182602a36d391F7F16DD9f8386C3bD8D"; // Replace with the actual CFAv1 Forwarder contract address
 
 const Home = () => {
+  const [open, setOpen] = useState(false);
+  const [monthlyFlowRate, setmonthlyFlowRate] = useState<Number>();
+  const [flowRate, setFlowRate] = useState<string>("");
+  // const { address, isConnected } = useAccount();
+
+  const { writeContract } = useWriteContract();
+
+  const openDialog = () => {
+    setOpen(true);
+  };
+
+  const closeDialog = () => {
+    setOpen(false);
+  };
+
+  const onMonthlyFlowRateChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setmonthlyFlowRate(e.target.value);
+  };
+
+  // useEffect(() => {
+  //   if (monthlyFlowRate) {
+  //     const monthlyFlowRateValue = Number(monthlyFlowRate);
+  //     if (!isNaN(monthlyFlowRateValue)) {
+  //       const normalizedOutflowRate = (
+  //         (monthlyFlowRateValue * 1e18) /
+  //         ((60 * 60 * 24 * 365) / 12)
+  //       ).toFixed(0);
+  //       setFlowRate(normalizedOutflowRate);
+  //     }
+  //   }
+  // }, [monthlyFlowRate]);
+
+  // const startStream = (e: )
+
   return (
     <>
       <Box textAlign="center" className="box-container">
+        <Modal
+          isOpen={open}
+          onClose={closeDialog}
+          // PaperProps={{
+          //   component: "form",
+          //   onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
+          //     event.preventDefault();
+          //     const formData = new FormData(event.currentTarget);
+          //     const formJson = Object.fromEntries((formData as any).entries());
+          //     const email = formJson.email;
+          //     console.log(email);
+          //     closeDialog();
+          //   },
+          // }}
+        >
+          <ModalContent>
+            <ModalHeader>Your way to freedom</ModalHeader>
+            <ModalBody>
+              To start working honestly, we kindly ask you to give us your money
+              by creating a stream. Select the amount of AVAX you want to stream
+              monthly.
+              {/* <TextField
+                value={monthlyFlowRate}
+                onChange={onMonthlyFlowRateChange}
+              /> */}
+              <NumberInput
+                onChange={(valueString) =>
+                  setmonthlyFlowRate(parse(valueString))
+                }
+                value={format(value)}
+                max={50}
+              >
+                <NumberInputField />
+              </NumberInput>
+            </ModalBody>
+            <ModalFooter>
+              <Button onClick={closeDialog}>Cancel</Button>
+              <Button
+                type="submit"
+                onClick={() =>
+                  writeContract({
+                    //@ts-ignore
+                    CFAv1ForwarderABI,
+                    address: CFAv1ForwarderAddress,
+                    functionName: "createFlow",
+                    args: [
+                      "0xfFD0f6d73ee52c68BF1b01C8AfA2529C97ca17F3",
+                      "0x178A621F2bbC191f8819e4a9C08C85Ce007D2094", // change for real contract address
+                      flowRate,
+                    ],
+                  })
+                }
+              >
+                Create stream
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
         <div className="left">
           <div className="container top-left">
             <h1>Game rules</h1>
@@ -35,8 +142,9 @@ const Home = () => {
           </div>
           <div className="container bottom-right">
             <h1>Actions</h1>
-            <button>Contribute more</button>
-            <button>Invite</button>
+            <Button data-cy={"open-dialog"} onClick={openDialog}>
+              Start
+            </Button>
           </div>
         </div>
       </Box>
